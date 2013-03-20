ce34e30ecba202aa32166bcbffe848a85e249a3e
Liutaihua <defage@gmail.com>
2013-03-20 22:46:01 +0800

    update

--- app/controllers/welcome.py
+++ app/controllers/welcome.py
@@ -160,7 +160,7 @@ class welcome_confirm_email:
                 users.update_confirm_email(token)
 
                 #如果这个用户已经在user表中存在，则是旧用户
-                if not users.is_douban_id_available(douban_id):
+                if users.is_douban_id_exist(douban_id):
                     #把邮件地址 等 用户信息 更新到 user 表
                     users.update_user_by_douid(
                         douban_id,
